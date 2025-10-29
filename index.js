5c9cbc2464f3a914da290590fc8e3c1998b3824c
Your Name <danamonuraalhaji@gmail.com>
2025-10-29 20:29:35 +0100

    commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const mongoose = require("mongoose")
 
 
 const corsConfig = {
-    origin : ["https://attasfiyah-portal.vercel.app"],
+    origin : ["*"],
     credential : true,
     methods : ["GET","POST","PUT","DELETE"],
     headers:["*"]
@@ -38,41 +38,6 @@ app.get("/",(req,res)=>{
 
 
 app.listen(Port, ()=>{
-const subjects=[
-    {
-      "name": "QUR'AN",
-      "CA1": 5,
-      "CA2": 6,
-      "Ass": 4,
-      "Exam": 55
-    },
-    {
-      "name": "TAJWEED",
-      "CA1": 7,
-      "CA2": 5,
-      "Ass": 6,
-      "Exam": 54
-    }
-  ]
-    const transformedScores = subjects.reduce((accumulator, subject) => {
-    // A. Clean Key Generation
-    const subjectKey = subject.name.toUpperCase().replace("'", ""); // QUR'AN -> QURAN
-
-    // B. Isolate Scores
-    // Destructure the subject object: 'name' is discarded, everything else goes into 'scores'.
-    const { name, ...scores } = subject; // scores = { CA1: 5, CA2: 6, Ass: 4, Exam: 55 }
-
-    // C. Mapping to the Accumulator
-    // The key is the subject name, and the value is the scores object wrapped in an array [].
-    accumulator[subjectKey] = [scores];
-
-    return accumulator;
-}, {}); // Start with an empty object {}
-
-/*
-'transformedScores' now holds:
-{ "QURAN": [ { CA1: 5, CA2: 6, ... } ], "TAJWEED": [ { CA1: 7, CA2: 5, ... } ] }
-*/ console.log(transformedScores)
     console.log("server is Running")
     
 })
