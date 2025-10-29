914b87473e9e88286d2c93cba7c63d2ad8b9f357
nuraalhaji <you@example.com>
2025-10-29 11:54:07 +0100

    commit

--- controllers/controller.js
+++ controllers/controller.js
@@ -3,7 +3,29 @@ const { Portal} = require("../model/portal")
 
 
 const postStudent = async(req,res) => {
-    const { } = req.body
+    const {school,studentName, classes, term, session, admissionNo, sex,subjects,age } = req?.body
+
+    const sub = subjects.reduce((accumulator, subject) => {
+    // A. Clean Key Generation
+    const subjectKey = subject.name.toUpperCase().replace("'", ""); // QUR'AN -> QURAN
+
+    // B. Isolate Scores
+    // Destructure the subject object: 'name' is discarded, everything else goes into 'scores'.
+    const { name, ...scores } = subject; // scores = { CA1: 5, CA2: 6, Ass: 4, Exam: 55 }
+
+    // C. Mapping to the Accumulator
+    // The key is the subject name, and the value is the scores object wrapped in an array [].
+    accumulator[subjectKey] = [scores];
+
+    return accumulator;
+}, {}); // Start with an empty object {}
+
+/*
+'transformedScores' now holds:
+{ "QURAN": [ { CA1: 5, CA2: 6, ... } ], "TAJWEED": [ { CA1: 7, CA2: 5, ... } ] }
+*/ 
+
+
     await Portal.create({
     school: school,
     studentName:studentName,
@@ -14,15 +36,15 @@ const postStudent = async(req,res) => {
     age: age,
     sex: sex,
     
-    QURAN:[{ CA1:CA1, CA2:CA2, Ass:Ass, Exam: Exam}],
-    TAJWEED:[{CA1:CA1, CA2:CA2, Ass:Ass,  Exam: Exam}],
-    TAUHEED:[{CA1:CA1, CA2:CA2, Ass:Ass,  Exam: Exam}],
-    FIQH:[{CA1:CA1,  CA2:CA2, Ass:Ass, Exam: Exam}],
-    HADITH:[{CA1:CA1, CA2:CA2, Ass:Ass,  Exam: Exam}],
-    ARABIC:[{CA1:CA1, CA2:CA2, Ass:Ass,  Exam: Exam}],
-    AZKHAR:[{CA1:CA1, CA2:CA2, Ass:Ass,  Exam: Exam}],
-    SIRAH:[{CA1:CA1, CA2:CA2, Ass:Ass,  Exam: Exam}],
-    HURUF:[{CA1:CA1, CA2:CA2, Ass:Ass,  Exam: Exam}],
+    QURAN:[{ CA1:sub.QURAN.CA1, CA2:sub.QURAN.CA2, Ass:sub.QURAN.Ass, Exam: sub.QURAN.Exam}],
+    TAJWEED:[{CA1:sub.TAJWEED.CA1, CA2:sub.TAJWEED.CA2, Ass:sub.TAJWEED.Ass,  Exam:sub.TAJWEED.Exam}],
+    TAUHEED:[{CA1:sub.TAUHEED.CA1, CA2:sub.TAUHEED.CA2, Ass:sub.TAUHEED.Ass,  Exam:sub.TAUHEED.Exam}],
+    FIQH:[{CA1:sub.FIQH.CA1,  CA2:sub.FIQH.CA2, Ass:sub.FIQH.Ass, Exam:sub.FIQH.Exam}],
+    HADITH:[{CA1:sub.HADITH.CA1, CA2:sub.HADITH.CA2, Ass:sub.HADITH.Ass,  Exam:sub.HADITH.Exam}],
+    ARABIC:[{CA1:sub.ARABIC.CA1, CA2:sub.ARABIC.CA2, Ass:sub.ARABIC.Ass,  Exam:sub.ARABIC.Exam}],
+    AZKHAR:[{CA1:sub.AZKHAR.CA1, CA2:sub.AZKHAR.CA2, Ass:sub.AZKHAR.Ass,  Exam:sub.AZKHAR.Exam}],
+    SIRAH:[{CA1:sub.SIRAH.CA1, CA2:sub.SIRAH.CA2, Ass:sub.SIRAH.Ass,  Exam:sub.SIRAH.Exam}],
+    HURUF:[{CA1:sub.HURUF.CA1, CA2:sub.HURUF.CA2, Ass:sub.HURUF.Ass,  Exam:sub.HURUF.Exam}],
 
     
 
