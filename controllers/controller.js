ac67af6d9547585bc88d719c05c6a2c2e95a3fae
nuraalhaji <you@example.com>
2025-10-31 10:45:48 +0100

    commit

--- controllers/controller.js
+++ controllers/controller.js
@@ -56,7 +56,7 @@ const getAllStudent = async (req,res) =>{
 const getOneStudent =  async(req,res)=>{
     try{
     const {_id} = req.params;
-    const student = await Student.findById(_id)
+    const student = await Portal.findById(_id)
     res.status(200).json(student)
     }catch(err){
         res.status(500).json({message: err.message})
@@ -67,7 +67,7 @@ const putOneStudent =  async(req,res)=>{
     try {
        
         const {_id} = req.params
-        const student = await Student.findByIdAndUpdate({_id:_id}, req.body)
+        const student = await Portal.findByIdAndUpdate({_id:_id}, req.body)
         
         if(!student){
             res.status(404).json("student not found")
@@ -84,7 +84,7 @@ const putPullStudent = async (req,res) => {
     const {_id2} = req.params;
     const {object} = req.params
 
-    const student =  await Student.findOneAndUpdate({_id:_id},
+    const student =  await Portal.findOneAndUpdate({_id:_id},
         {$pull:
           {[`${object}`]:{_id:_id2}}
       })
@@ -92,25 +92,15 @@ const putPullStudent = async (req,res) => {
                     
 }
 
-const putPushStudent = async (subjects,id) => {
+const putPushStudent = async (req,res) => {
+    const {object,id} = req.params;
+    const {CA1,CA2,Ass,Exam} =req.body
 try {
     
-        await Portal.findByIdAndUpdate({_id:id?._id},{
-        $push:{
-        QURAN:[{ CA1:subjects?.QURAN.CA1, CA2:subjects?.QURAN.CA2, Ass:subjects?.QURAN.Ass, Exam:subjects?.QURAN.Exam}],
-        TAJWEED:[{CA1:subjects?.TAJWEED.CA1, CA2:subjects?.TAJWEED.CA2, Ass:subjects?.TAJWEED.Ass,  Exam:subjects?.TAJWEED.Exam}],
-        TAUHEED:[{CA1:subjects?.TAUHEED.CA1, CA2:subjects?.TAUHEED.CA2, Ass:subjects?.TAUHEED.Ass,  Exam:subjects?.TAUHEED.Exam}],
-        FIQH:[{CA1:subjects?.FIQH.CA1,  CA2:subjects?.FIQH.CA2, Ass:subjects?.FIQH.Ass, Exam:subjects?.FIQH.Exam}],
-        HADITH:[{CA1:subjects?.HADITH.CA1, CA2:subjects?.HADITH.CA2, Ass:subjects?.HADITH.Ass,  Exam:subjects?.HADITH.Exam}],
-        ARABIC:[{CA1:subjects?.ARABIC.CA1, CA2:subjects?.ARABIC.CA2, Ass:subjects?.ARABIC.Ass,  Exam:subjects?.ARABIC.Exam}],
-        AZKHAR:[{CA1:subjects?.AZKHAR.CA1, CA2:subjects?.AZKHAR.CA2, Ass:subjects?.AZKHAR.Ass,  Exam:subjects?.AZKHAR.Exam}],
-        SIRAH:[{CA1:subjects?.SIRAH.CA1, CA2:subjects?.SIRAH.CA2, Ass:subjects?.SIRAH.Ass,  Exam:subjects?.SIRAH.Exam}],
-        HURUF:[{CA1:subjects?.HURUF.CA1, CA2:subjects?.HURUF.CA2, Ass:subjects?.HURUF.Ass,  Exam:subjects?.HURUF.Exam}],
+        await Portal.findByIdAndUpdate({_id:id},{
+        $push:{ [`${object}`]:[{ CA1:CA1, CA2:CA2, Ass:Ass, Exam:Exam}], }})
 
-    
-        }
-    })
-     console.log("is working")
+    res.json("successful")
     } catch (error) {
         console.log(error) 
      }  
@@ -121,7 +111,7 @@ const putSetStudent = async (req,res) => {
     const {key} = req.params;
     const {index} = req.params;
     const {value} = req.body;
-    const student =  await Student.findOneAndUpdate({_id:_id},
+    const student =  await Portal.findOneAndUpdate({_id:_id},
         {$set:
           {
            [`${object}.${index}.${key}`]:value
@@ -133,7 +123,7 @@ const putSetStudent = async (req,res) => {
 const deleteOneStudent =  async(req,res)=>{
     try {
         const {_id}=req.params
-        const student = await Student.findByIdAndDelete({_id:_id}, req.body)
+        const student = await Portal.findByIdAndDelete({_id:_id}, req.body)
 
         if(!student){
             res.status(404).json("student not found")
--- routers/router.js
+++ routers/router.js
@@ -12,7 +12,7 @@ const { postStudent,putSetStudent,putPullStudent, putPushStudent, deleteOneStude
 studentRouter.post('/create', postStudent)
 studentRouter.get('/', getAllStudent)
 studentRouter.get('/:_id', getOneStudent)
-studentRouter.put('/push/:_id/:object', putPushStudent)
+studentRouter.put('/push/:id/:object', putPushStudent)
 studentRouter.put('/:_id', putOneStudent)
 studentRouter.put('/pull/:_id/:_id2/:object', putPullStudent)
 studentRouter.put('/set/:_id/:object/:index/:key', putSetStudent)
