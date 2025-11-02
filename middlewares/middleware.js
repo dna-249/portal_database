d041640e8ba6754c5d20651dcdfeabaf75e2a622
Your Name <danamonuraalhaji@gmail.com>
2025-11-02 22:21:09 +0100

    commit

--- middlewares/middleware.js
+++ middlewares/middleware.js
@@ -2,13 +2,13 @@ const jwt = require("jsonwebtoken")
 const {Portal} = require("../model/portal")
 exports.studentLogin = async (req,res,next) =>{
   try {
-    const {name, password} = req.body;
-    const student = await Portal.findOne({name:name,password:password})
+    const {studentName, password} = req.body;
+    const student = await Portal.findOne({studentName:studentName,password:password})
     if(!student) {
         res.status(404).json("not found")
     }
-    const token = jwt.sign({name:student.name,password:student.password},process.env.secret)
-    res.send(res.json(token)) 
+    const token = jwt.sign({studentName:student.studentName,password:student.password},process.env.secret)
+    res.json(token) 
     next()
   } catch (error) {
    console.log(error) 
--- middlewares/verify.js
+++ middlewares/verify.js
@@ -2,18 +2,18 @@ const jwt = require("jsonwebtoken")
 const {Portal} =require("../model/portal")
 
 exports.studentVerify = async (req,res,next)=>{
-    const {header,name,password} = req.body
+    const {header,studentName,password} = req.body
 
     try {
        const token = await header;
-       const student = await Portal.findOne({name:name,password:password})
+       const student = await Portal.findOne({studentName:studentName,password:password})
        if(!token){
         console.log("access denied")
        } 
        const verified = jwt.verify(token, process.env.secret)
-       req.name = verified;
+       req.studentName = verified;
        req.password = verified;
-       res.send(res.json(student))
+       res.json(student)
        next()
     } catch (error) {
         console.log(error)
